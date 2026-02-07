b00a822ecf454a60b844deda35ff2fbf5114b61d
ali-2004-byte <ah5231114@gmail.com>
2026-02-07 21:47:21 +0500

    Add graph generation from bounding box using osmnx

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import osmnx as ox
 import networkx as nx
-print("Hello, World!")
+
+bbox = (33.72, 33.64, 73.10, 73.00)
+
+G = ox.graph_from_bbox(
+    bbox,
+    network_type="drive"
+)
+print(f"Nodes: {G.number_of_nodes()}, Edges: {G.number_of_edges()}")
